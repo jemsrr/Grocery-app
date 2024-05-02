aa354fc9969f234905d2d73bbc344d987faf164c
jemsrr <jemsrakholiya4949@gmail.com>
2024-05-02 15:09:58 +0530

    add logout

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -85,7 +85,7 @@ const Login = () => {
             Login Now
           </button>
           <p className="text-center mt-4">
-            Don't have an account?
+            Don&apos;t have an account?
             <Link href="/signup" className="text-blue-500 hover:underline">
               SignUp
             </Link>
